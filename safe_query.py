40c7e67b63a48c489e82e43ec894fc0776547057
AbhisekSingh <54771322+singhabhisek@users.noreply.github.com>
2025-08-20 00:37:58 -0400

    Update safe_query.py

--- safe_query.py
+++ safe_query.py
@@ -1,1 +1,10 @@
 SELECT * FROM orders WHERE order_id = '12345';
+# false_positive_py1.py
+import os
+
+# Safe constant string
+user_input = "HelloWorld"
+
+# Using f-string
+cmd = f"echo {user_input}"
+os.system(cmd)  # Might appear risky to CodeQL
