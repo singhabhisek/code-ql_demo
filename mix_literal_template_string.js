930924cf78ec87491a175a9ca3b36e227d313b8d
AbhisekSingh <54771322+singhabhisek@users.noreply.github.com>
2025-08-20 09:13:58 -0400

    Update mix_literal_template_string.js

--- mix_literal_template_string.js
+++ mix_literal_template_string.js
@@ -1,5 +1,6 @@
 const userInput = "static";  // not real user input
 const { exec } = require("child_process");
+//abcd comment
 exec(`echo ${userInput}`, (err, stdout) => {
   if (err) console.error(err);
   console.log(stdout);
