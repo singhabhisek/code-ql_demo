d2f82a274eabb68ba75266164e43266c977241c4
AbhisekSingh <54771322+singhabhisek@users.noreply.github.com>
2025-08-20 00:34:54 -0400

    Update testjs.js

--- web/testjs.js
+++ web/testjs.js
@@ -1,7 +1,7 @@
 // test_vuln.js
 const { exec } = require("child_process");
 const http = require("http");
-
+//
 http.createServer((req, res) => {
     const userInput = req.url; // user-controlled input
     exec(`echo ${userInput}`, (err, stdout, stderr) => {
